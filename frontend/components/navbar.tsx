66539a746f874c03e9af10e128991f55035badb6
Aditya Khalkar <22510466.dypit@dypvp.edu.in>
2025-04-06 01:29:28 +0530

    small changes

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -157,7 +157,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="relative backdrop-filter backdrop-blur-xl z-50">
+    <nav className="relative backdrop-filter backdrop-blur-xl bg-black z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           {/* Logo Section */}
